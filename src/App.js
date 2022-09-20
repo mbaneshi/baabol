1248b4b76bdade86d401df5fa019a13141baf485
Mehdi Baneshi <baneshi@gmail.com>
2022-09-20 19:20:20 +0430

    add subtiitl ,delet svg

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import classes from "./App.module.css";
 function App() {
   return (
     <div className={classes.mostOutherDiv}>
-      <h1>Hello World</h1>
-      <h3> This is subtitle</h3>
+
+    <h1>
+    Hello World
+    </h1>
+      <h1 className={classes.header}>Hello World</h1>
+      <h3 className={classes.subtitle}> This is subtitle</h3>
     </div>
   );
 }
